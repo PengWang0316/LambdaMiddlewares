c7e77105f2fd041b972ed92027a957a1c0b1690d
Kevin Wang <rocwang0316@gmail.com>
2019-02-05 11:40:01 -0800

    Added the mongoSanitize function.

--- src/lambda-middlewares.js
+++ src/lambda-middlewares.js
@@ -1,5 +1,6 @@
 import cloudwatch from '@kevinwang0316/cloudwatch';
 import { initialConnects } from '@kevinwang0316/mongodb-helper';
+import sanitize from 'mongo-sanitize';
 
 // The middleware to flush the metrics to the CloudWatch
 export const flushMetrics = {
@@ -10,5 +11,23 @@ export const flushMetrics = {
 export const initializeMongoDB = {
   before: (handler, next) => {
     initialConnects(handler.context.dbUrl, handler.context.dbName).then(() => next());
+  },
+};
+
+// Sanitize the user input parameters or body
+export const mongoSanitize = {
+  before: (handler, next) => {
+    const { queryStringParameters, body } = handler.event;
+    if (queryStringParameters) { // If the event has queryStringParameters, sanitzie all of the element
+      const newParameters = {};
+      Object.keys(queryStringParameters).forEach(key => { newParameters[key] = sanitize(queryStringParameters[key]); });
+      handler.queryStringParameters = newParameters;
+    }
+    if (body) {
+      const newBody = {};
+      Object.keys(body).forEach(key => { newBody[key] = sanitize(body[key]); });
+      handler.body = newBody;
+    }
+    next();
   },
 };

73bd6c8e50e2b29dc11435a8e2e86e03843bb3dc
Kevin Wang <rocwang0316@gmail.com>
2019-02-05 11:43:13 -0800

    Added sampleLogging function

--- src/lambda-middlewares.js
+++ src/lambda-middlewares.js
@@ -1,6 +1,7 @@
 import cloudwatch from '@kevinwang0316/cloudwatch';
 import { initialConnects } from '@kevinwang0316/mongodb-helper';
 import sanitize from 'mongo-sanitize';
+import log from '@kevinwang0316/log';
 
 // The middleware to flush the metrics to the CloudWatch
 export const flushMetrics = {
@@ -30,4 +31,33 @@ export const mongoSanitize = {
     }
     next();
   },
+};
+
+export const sampleLogging = (option = { sampleRate: 0.01 }) => { // The defualt sample rate is 1%
+  let logLevel;
+  const { sampleRate } = option;
+
+  // const isDebugEnabled = () => {
+  //   return correlationIds.get()['Debug-Log-Enabled'] === 'true' ? true : Math.random() <= sampleRate;
+  // };
+
+  return {
+    before: (handler, next) => {
+      if (process.env.log_level !== 'DEBUG' && Math.random() <= sampleRate) {
+        logLevel = process.env.log_level;
+        process.env.log_level = 'DEBUG';
+      }
+      next();
+    },
+    after: (handler, next) => {
+      if (logLevel) process.env.log_level = logLevel;
+      next();
+    },
+    onError: (handler, next) => { // Handle the error.
+      const { awsRequestId } = handler.context;
+      const invocationEvent = JSON.stringify(handler.event);
+      log.error('Invocation failed', { awsRequestId, invocationEvent }, handler.error);
+      next(handler.error);
+    },
+  };
 };
